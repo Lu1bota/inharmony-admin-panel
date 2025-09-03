71441fc0ba6c892a1c3b717e34e8a3a02bf16c1f
Danylo Liubota <daniil.lyubota@gmail.com>
2025-09-03 16:44:13 +0300

    feat: add requests api

---
+++ lib/api.ts
@@ -0,0 +1,347 @@
+import {
+  Collection,
+  CreateCollectionRequest,
+  GetCollectionByIdParams,
+  getReportsRes,
+  Locale,
+  loginUserBody,
+  MerchBody,
+  MerchReq,
+  PaginatedCollectionsResponse,
+  PartnerBody,
+  PartnersRes,
+  ReportReq,
+  TeammateBody,
+  Teammates,
+  TeammatesRes,
+  UpdateCollectionRequest,
+  UserBody,
+  UserRes,
+} from "@/types";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://inharmony-v1.h.goit.study/api",
+  withCredentials: true,
+});
+
+// Auth
+
+export async function loginUser(payload: loginUserBody) {
+  try {
+    const res = await api.post<{ message: string }>("/auth/login", payload);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function logoutUser() {
+  try {
+    await api.post("/auth/logout");
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function registerUser(payload: UserBody) {
+  try {
+    const res = await api.post<UserRes>("/auth/register", payload);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getUsers() {
+  try {
+    const res = await api.get<UserRes>("/auth/users");
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function updateUserById(id: string, payload: UserBody) {
+  try {
+    const res = await api.patch<UserRes>(`/auth/users/${id}`, payload);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deleteUser(id: string) {
+  try {
+    await api.delete(`/auth/users/${id}`);
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Collections
+
+export async function getCollections(
+  locale: string,
+  page: number = 1,
+  perPage: number = 6
+) {
+  try {
+    const res = await api.get<PaginatedCollectionsResponse>(
+      `/collections/${locale}`,
+      {
+        params: {
+          page,
+          perPage,
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getCollectionById({
+  locale,
+  id,
+}: GetCollectionByIdParams) {
+  try {
+    const res = await api.get<Collection>(`/collections/${locale}/${id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getCollectionsTags() {
+  try {
+    const res = await api.get("/collections/tags");
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createCollection(
+  locale: Locale,
+  payload: CreateCollectionRequest
+) {
+  try {
+    const formData = new FormData();
+
+    Object.entries(payload).forEach(([key, value]) => {
+      if (key === "long_desc" && Array.isArray(value)) {
+        value.forEach((item, index) => {
+          formData.append(`long_desc[${index}]`, item);
+        });
+      } else if (value !== undefined && value !== null) {
+        formData.append(key, value);
+      }
+    });
+
+    const res = await api.post<Collection>(`/collections/${locale}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function updateCollection(
+  locale: Locale,
+  id: string,
+  payload: UpdateCollectionRequest
+) {
+  try {
+    const formData = new FormData();
+
+    Object.entries(payload).forEach(([key, value]) => {
+      if (key === "long_desc" && Array.isArray(value)) {
+        value.forEach((item, index) => {
+          formData.append(`long_desc[${index}]`, item);
+        });
+      } else if (value !== undefined && value !== null) {
+        formData.append(key, value);
+      }
+    });
+
+    const res = await api.patch<Collection>(
+      `/collections/${locale}/${id}`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deleteCollection(locale: Locale, id: string) {
+  try {
+    await api.delete(`/collections/${locale}/${id}`);
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Reports
+
+export async function getReports(locale: Locale) {
+  try {
+    const res = await api.get<getReportsRes>("/reports", { params: locale });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function updatePerort(id: string, payload: ReportBody) {
+  try {
+    const res = await api.patch<ReportReq>(`/reports/${id}`, payload);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createReport(payload: ReportBody) {
+  try {
+    const res = await api.post<ReportReq>("/reports", payload);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deleteReport(id: string) {
+  try {
+    await api.delete(`reports/${id}`);
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Partners
+
+export async function getPartners() {
+  try {
+    const res = await api.get<PartnersRes>("/partners");
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function updatePartner(id: string, payload: PartnerBody) {
+  try {
+    const res = await api.patch(`/partners/${id}`, payload, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createPartner(payload: PartnerBody) {
+  try {
+    const res = await api.post("/partners", payload, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function deletePartner(id: string) {
+  try {
+    await api.delete(`/partners/${id}`);
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Merch
+
+export async function getMerch() {
+  try {
+    const res = await api.get<MerchReq[]>("/merch");
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// баг при запиті
+export async function updateMerch(locale: Locale, payload: MerchBody) {
+  try {
+    const res = await api.patch<MerchReq>(`/merch/${locale}`, payload);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Teammates
+
+export async function getTeammates(locale: Locale) {
+  try {
+    const res = await api.get<TeammatesRes>("/teammates", { params: locale });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// баг при запиті, додати тип res, locale not alloved
+export async function updateTeammate(
+  locale: Locale,
+  id: string,
+  payload: TeammateBody
+) {
+  try {
+    const res = await api.patch(`/teammates/${id}`, payload, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createTeammate(payload: TeammateBody) {
+  try {
+    const res = await api.post<Teammates>("/teammates", payload, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// 400 Invalid id для існуючих тімейтів
+export async function deleteTeammate(locale: Locale, id: string) {
+  try {
+    await api.delete(`/teammates/${id}`, { params: locale });
+  } catch (error) {
+    throw error;
+  }
+}
---
+++ types/index.ts
@@ -0,0 +1,217 @@
+// Auth Types
+
+export type registerRole = "editor" | "admin";
+
+export interface UserRes {
+  email: string;
+  role: registerRole;
+  name: string;
+  _id: string;
+}
+
+export interface UserBody {
+  name: string;
+  email: string;
+  password: string;
+  role: registerRole;
+}
+
+export interface loginUserBody {
+  email: string;
+  password: string;
+}
+
+// Collections Types
+
+export interface CollectionImage {
+  url: string;
+  path: string;
+}
+
+export interface LongDescription {
+  section1: string;
+  section2: string;
+  section3: string;
+  _id: string;
+}
+
+export interface Collection {
+  _id: string;
+  title: string;
+  image: CollectionImage[];
+  collected: number;
+  target: number;
+  alt: string;
+  peopleDonate: number;
+  peopleDonate_title: string;
+  desc: string;
+  closedAt: string | null;
+  language: string;
+  collected_title: string;
+  target_title: string;
+  term: string | null;
+  days: number | null;
+  period: string | null;
+  comments: string[] | null;
+  quantity: number | null;
+  currency: string | null;
+  long_desc: LongDescription;
+  status: "active" | "closed";
+  type: string;
+  value: string;
+  importance: "urgent" | "important" | "non-urgent" | "permanent";
+  createdAt: string;
+  __v: number;
+  translations: string;
+}
+
+export interface CollectionsResponse {
+  activeCollections: Collection[];
+  closedCollections: Collection[];
+}
+
+export interface PaginatedCollectionsResponse {
+  status: number;
+  data: CollectionsResponse;
+  pagination: {
+    totalItems: number;
+    totalPages: number;
+    currentPage: number;
+    perPage: number;
+  };
+}
+
+export type Locale = "en" | "ua";
+
+export interface GetCollectionByIdParams {
+  locale: Locale;
+  id: string;
+}
+
+export interface CreateCollectionRequest {
+  title: string;
+  image: File;
+  collected: number;
+  target: number;
+  alt: string;
+  peopleDonate: number;
+  peopleDonate_title: string;
+  desc: string;
+  days?: number;
+  period: string;
+  quantity?: number;
+  status?: "active" | "closed";
+  value: string;
+  importance: "urgent" | "important" | "non-urgent" | "permanent";
+  long_desc: string[];
+}
+
+export interface UpdateCollectionRequest {
+  title?: string;
+  collected?: number;
+  target?: number;
+  alt?: string;
+  peopleDonate?: number;
+  peopleDonate_title?: string;
+  desc?: string;
+  days?: number;
+  period?: string;
+  quantity?: number;
+  status?: "active" | "closed";
+  value?: string;
+  importance?: "urgent" | "important" | "non-urgent" | "permanent";
+  long_desc?: string[];
+  image?: File;
+}
+
+// Reports Types
+
+export interface getReportsRes {
+  _id: string;
+  month: string;
+  year: string;
+  url: string;
+  type: string;
+  language: Locale;
+  status: string;
+}
+
+export interface ReportBody {
+  year: string;
+  month: string;
+  url: string;
+  language: Locale;
+}
+
+export interface ReportReq {
+  id: string;
+  month: string;
+  year: string;
+  url: string;
+  language: Locale;
+}
+
+// Partners Types
+
+export interface Image {
+  url: string;
+  path: string;
+}
+
+export interface PartnersRes {
+  _id: string;
+  logo: string;
+  type: string;
+  image: Image[];
+  link: string;
+  language: Locale;
+}
+
+export interface PartnerBody {
+  image: File;
+  logo: string;
+  link: string;
+  language: Locale;
+}
+
+// Merch Types
+
+export type Status = "off" | "on";
+
+export interface MerchReq {
+  _id: string;
+  status: Status;
+  content: string;
+  link: string;
+  locale: Locale;
+}
+
+export interface MerchBody {
+  status: Status;
+  content: string;
+  link: string;
+}
+
+// Teammates Types
+
+export interface Teammates {
+  icon: string;
+  name: string;
+  role: string;
+  description: string;
+  _id: string;
+  image: Image[];
+}
+
+export interface TeammatesRes {
+  locale: Locale;
+  teammates: Teammates[];
+}
+
+export interface TeammateBody {
+  name: string;
+  role: string;
+  description: string;
+  image: File;
+  locale: Locale;
+}
